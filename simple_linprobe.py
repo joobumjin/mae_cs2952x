cbf4d7632d3695fd8701fa9bae27d22077efec77
joobumjin <bumjin.joo@gmail.com>
2025-10-09 15:00:40 -0400

    fix: detaching

--- simple_linprobe.py
+++ simple_linprobe.py
@@ -96,7 +96,9 @@ def test(model: torch.nn.Module, probe: torch.nn.Module, data_loader: Iterable, 
 
         with torch.no_grad():
             embeds, _, _ = model.forward_encoder(samples["image"], 0)
+            print(embeds.shape)
             loss, preds = probe(embeds, samples["label"])
+            print(preds.shape)
 
         metrics["Test Loss"].update(loss.item())
 
