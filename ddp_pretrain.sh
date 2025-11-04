174a6aa6b4ce27dd22cb990891f3df894c3a2aba
joobumjin <bumjin.joo@gmail.com>
2025-11-04 18:23:08 -0500

    diff partition

--- ddp_pretrain.sh
+++ ddp_pretrain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu --gres=gpu:2 --output=mae_ddp.out
+#SBATCH --partition=norm-gpu --gres=gpu:2 --output=mae_ddp.out
 #SBATCH -N 2
 #SBATCH -n 2
 #SBATCH -t 00:30:00
